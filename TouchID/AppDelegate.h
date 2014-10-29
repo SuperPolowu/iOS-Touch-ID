514b1126fbc8bee27bef1e10f6087ecce5a2f922
SuperPolowu <polowu7581@gmail.com>
2014-10-29 17:14:12 +0800

    Initial Commit

---
+++ TouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchID
+//
+//  Created by Polo on 2014/10/29.
+//  Copyright (c) 2014年 Polo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchID/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TouchID
+//
+//  Created by Polo on 2014/10/29.
+//  Copyright (c) 2014年 Polo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
